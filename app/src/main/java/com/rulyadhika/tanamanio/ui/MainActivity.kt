e52f444ec869f603c249f802eb6db9392d95101d
rulyadhika <rulihanif@gmail.com>
2023-10-13 10:32:34 +0700

    add rectangle_hitz_plant_card, add hitz plant section on main screen

--- app/src/main/java/com/rulyadhika/tanamanio/ui/MainActivity.kt
+++ app/src/main/java/com/rulyadhika/tanamanio/ui/MainActivity.kt
@@ -17,10 +17,12 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
     private lateinit var btnSeeAllOwnedPlant: Button
     private lateinit var llFewOwnedPlant: LinearLayout
     private lateinit var llPlantCategory: LinearLayout
+    private lateinit var llFewHitzPlants: LinearLayout
     private lateinit var btnAboutMe: Button
 
     private var listFewOwnedPlants = ArrayList<Plant>()
     private var listPlantsCategory = ArrayList<PlantCategory>()
+    private var listFewHitzPlants = ArrayList<Plant>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -62,10 +64,11 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
                     view.context.startActivity(intent)
                 }
             }
-        }else{
-            val view = LayoutInflater.from(this@MainActivity).inflate(R.layout.simple_text_message_with_picture,llFewOwnedPlant,false)
-            val tvSimpleMessage:TextView = view.findViewById(R.id.tv_message)
-            val ivPictureMessage:ImageView = view.findViewById(R.id.iv_picture_message)
+        } else {
+            val view = LayoutInflater.from(this@MainActivity)
+                .inflate(R.layout.simple_text_message_with_picture, llFewOwnedPlant, false)
+            val tvSimpleMessage: TextView = view.findViewById(R.id.tv_message)
+            val ivPictureMessage: ImageView = view.findViewById(R.id.iv_picture_message)
 
             tvSimpleMessage.text = getString(R.string.dont_have_any_plants)
             ivPictureMessage.setImageResource(R.drawable.sad_face_plant)
@@ -82,7 +85,8 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
                     .inflate(R.layout.square_plant_category_card, llPlantCategory, false)
 
                 val tvPlantCategory: TextView = view.findViewById(R.id.tv_plant_category)
-                val ivPlantCategoryPicture: ImageView = view.findViewById(R.id.iv_plant_category_picture)
+                val ivPlantCategoryPicture: ImageView =
+                    view.findViewById(R.id.iv_plant_category_picture)
 
                 tvPlantCategory.text = item.categoryName
                 ivPlantCategoryPicture.setImageResource(item.categoryImage)
@@ -90,6 +94,27 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
                 llPlantCategory.addView(view)
             }
         }
+
+        llFewHitzPlants = findViewById(R.id.ll_few_hitz_plants)
+        listFewHitzPlants.addAll(getFewHitzPlants())
+
+        if (listFewHitzPlants.size > 0) {
+            for (item in listFewHitzPlants) {
+                val view = LayoutInflater.from(this@MainActivity)
+                    .inflate(R.layout.rectangle_hitz_plant_card, llFewHitzPlants, false)
+
+                val ivPlantPicture : ImageView = view.findViewById(R.id.iv_plant_picture)
+                val tvPlantName : TextView = view.findViewById(R.id.tv_plant_name)
+                val tvPlantCategory : TextView = view.findViewById(R.id.tv_plant_category)
+
+                Glide.with(llFewHitzPlants).load(item.plantPicture).into(ivPlantPicture)
+                tvPlantName.text = item.plantName
+                tvPlantCategory.text = item.plantCategory
+
+                llFewHitzPlants.addView(view)
+            }
+        }
+
     }
 
     override fun onClick(v: View?) {
@@ -138,17 +163,49 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         return list
     }
 
-    private fun getListPlantsCategory() : ArrayList<PlantCategory>{
+    private fun getListPlantsCategory(): ArrayList<PlantCategory> {
         val dataName = resources.getStringArray(R.array.data_plant_category_name)
         val dataImage = resources.obtainTypedArray(R.array.data_plant_category_image)
 
         val resultArray = ArrayList<PlantCategory>()
 
-        for(i in dataName.indices){
-            val data = PlantCategory(dataName[i], dataImage.getResourceId(i,-1))
+        for (i in dataName.indices) {
+            val data = PlantCategory(dataName[i], dataImage.getResourceId(i, -1))
             resultArray.add(data)
         }
 
         return resultArray
+    }
+
+    private fun getFewHitzPlants(): ArrayList<Plant> {
+        val dataName = resources.getStringArray(R.array.data_name)
+        val dataLatinaName = resources.getStringArray(R.array.data_latina_name)
+        val dataDescription = resources.getStringArray(R.array.data_description)
+        val dataCategory = resources.getStringArray(R.array.data_category)
+        val dataDifficulty = resources.getStringArray(R.array.data_difficulty)
+        val dataPicture = resources.getStringArray(R.array.data_photo)
+        val dataHowToCare = resources.getStringArray(R.array.data_how_to_care)
+
+        var list = ArrayList<Plant>()
+
+        for (i in dataName.size - 1 downTo 0) {
+            if (list.size < 5) {
+                val plant = Plant(
+                    dataName[i],
+                    dataLatinaName[i],
+                    dataDescription[i],
+                    dataCategory[i],
+                    "${dataDifficulty[i]} Dirawat",
+                    dataPicture[i],
+                    dataHowToCare[i]
+                )
+
+                list.add(plant)
+            } else {
+                break
+            }
+        }
+
+        return list
     }
 }
